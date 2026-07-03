e2b062dde8f7bf9ac72a9e8119ed5090ebc2970e
growlab-id <officialgrowlab.id@gmail.com>
2026-07-03 21:22:26 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
                 t.src=v;s=b.getElementsByTagName(e)[0];
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('set', 'autoConfig', false, '${FB_PIXEL_ID}');   // ⬅️ baris baru
                 fbq('init', '${FB_PIXEL_ID}');
                 fbq('track', 'PageView');
               `}
